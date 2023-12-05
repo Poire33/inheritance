e5c20f67ef2d42d2f195b2967a9a22feba1d17a4
Rupert spelled with one P <g00423530@atu.ie>
2023-12-05 13:34:49 +0000

    Fixed psvm in Main.java

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,7 @@
 package ie.atu;
 
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         Person myPerson = new Person("Paul", "Kinvarra", "0974751313");
         System.out.println(myPerson.toString());
     }
