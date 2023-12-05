d6b1d25fdd7fca496f9eeef93d98d3b3d42ea65b
Rupert spelled with one P <g00423530@atu.ie>
2023-12-05 13:28:03 +0000

    Part a done ...?

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,8 @@
+package ie.atu;
+
+public class Main {
+    public static void main() {
+        Person myPerson = new Person("Paul", "Kinvarra", "0974751313");
+        System.out.println(myPerson.toString());
+    }
+}
